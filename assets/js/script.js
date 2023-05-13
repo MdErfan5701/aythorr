bef056037950e9bbb269db462c014acac8818183
MdErfan5701 <121732466+MdErfan5701@users.noreply.github.com>
2023-05-13 06:18:05 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(function () {
+   'use strict';
+   $('.banner-slider').slick({
+      prevArrow: '<i class=" banner-slider-arrow fas fa-arrow-left"></i>',
+      nextArrow: '<i class=" banner-slider-arrow fas fa-arrow-right"></i>'
+   })
+})
